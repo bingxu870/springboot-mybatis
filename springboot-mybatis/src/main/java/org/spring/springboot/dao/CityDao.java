279b8cda88697c4d11ea0ab051c45f14a7d4d989
bingxu870 <86406960+bingxu870@users.noreply.github.com>
2021-07-29 14:58:41 +0800

    Update CityDao.java

--- springboot-mybatis/src/main/java/org/spring/springboot/dao/CityDao.java
+++ springboot-mybatis/src/main/java/org/spring/springboot/dao/CityDao.java
@@ -12,7 +12,10 @@ public interface CityDao {
 
     /**
      * 根据城市名称，查询城市信息
-     *
+     *   
+     
+     
+     
      * @param cityName 城市名
      */
     City findByName(@Param("cityName") String cityName);
